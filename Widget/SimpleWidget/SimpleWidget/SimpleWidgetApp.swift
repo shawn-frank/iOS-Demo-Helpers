12e364b591ca086daf8518982b08538aaf5e765c
Shawn Frank <shawn@shawn.design>
2023-07-24 20:13:35 +1000

    init widget project

---
+++ Widget/SimpleWidget/SimpleWidget/SimpleWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWidgetApp.swift
+//  SimpleWidget
+//
+//  Created by Shawn Frank on 24/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
